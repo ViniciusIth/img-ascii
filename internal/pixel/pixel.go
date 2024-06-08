c95496a6e7076ac337704ccc75345c2d6fd17c55
ViniciusIth <vini.ithalo@hotmail.com>
2024-06-08 10:48:37 -0300

    fix: pixel brightness didn't take into consideration alpha channel

--- internal/pixel/pixel.go
+++ internal/pixel/pixel.go
@@ -10,5 +10,13 @@ type Pixel struct {
 }
 
 func GetPixelBrightness(pixel *Pixel) uint8 {
-	return uint8((0.2126 * float32(pixel.R)) + (0.7152 * float32(pixel.G)) + (0.0722 * float32(pixel.B)))
+	// Calculate brightness using one of the formulas for luminance
+	brightness := uint8((0.2126 * float32(pixel.R)) + (0.7152 * float32(pixel.G)) + (0.0722 * float32(pixel.B)))
+
+	// Adjust brightness based on alpha value
+    adjustedBrightness := float32(brightness) * (float32(pixel.A) / 255.0)
+
+    // Should I check if the brightness is greater than 255 or smaller than 0?
+    // I can't really see them being trespassed
+	return uint8(adjustedBrightness)
 }
