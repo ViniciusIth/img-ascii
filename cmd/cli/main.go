5e74ea25cea5bf2b28bba5f7afe43619c14af920
ViniciusIth <vini.ithalo@hotmail.com>
2024-06-08 10:50:25 -0300

    refactor: cli/main now accepts cmd arguments

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"os"
@@ -9,12 +10,26 @@ import (
 	_ "image/png"
 
 	"github.com/viniciusith/img2ascii/internal/converter"
-	image_util "github.com/viniciusith/img2ascii/internal/image"
 	"github.com/viniciusith/img2ascii/internal/pixel"
+	"github.com/viniciusith/img2ascii/internal/resize"
 )
 
 func main() {
-	file, _ := os.OpenFile("test.png", os.O_RDONLY, 0)
+
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Println("Please provide an image path")
+		fmt.Println("Usage: go run cmd/cli/main.go [options] <image_path>")
+		os.Exit(1)
+	}
+
+	width := flag.Int("width", 0, "Width of the image")
+
+	flag.Parse()
+
+	fmt.Println(*width)
+
+	file, _ := os.OpenFile(args[len(args)-1], os.O_RDONLY, 0)
 	defer file.Close()
 
 	img, _, err := image.Decode(file)
@@ -23,18 +38,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	img = image_util.ScaleWidth(img, 60)
+	if *width >= 80 {
+		img = resize.ScaleWidth(img, *width)
+	}
 
 	imgMatrix := converter.Image2RGBAMatrix(img)
 
 	saveFile, _ := os.Create("test.txt")
+
 	for y := 0; y < len(imgMatrix); y++ {
 		for x := 0; x < len(imgMatrix[y]); x++ {
-			converter.GetCharacterByBrightness(pixel.GetPixelBrightness(&imgMatrix[y][x]))
+			brightness := pixel.GetPixelBrightness(&imgMatrix[y][x])
+			char := converter.GetCharacterByBrightness(brightness)
 
-			fmt.Fprintf(saveFile, "%v", converter.GetCharacterByBrightness(pixel.GetPixelBrightness(&imgMatrix[y][x])))
+			fmt.Print(char)
+			fmt.Fprint(saveFile, char)
 		}
 
-        fmt.Fprintf(saveFile, "\n")
+		fmt.Println()
+		fmt.Fprintln(saveFile)
 	}
+
+	p := pixel.Pixel{R: 100, G: 100, B: 100, A: 0}
+	fmt.Println(pixel.GetPixelBrightness(&p)) // Output should be 54
 }
